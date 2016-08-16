eef679d72ffef0dd9320f40118059dad68e44a68
Francis <roccofrancis@gmail.com>
2016-08-16 15:07:59 -0400

    work on song new page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,22 @@
 class SongsController < ApplicationController
 
+#CREATE
+
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
+  post '/songs' do
+    binding.pry
+    song_artist_id = Artist.find_or_create_by(name: "#{params[:song][:artist][:name]}").id
+    song_genres = []
+    params[:song][:genres].each do |genre_id|
+      song_genres << Genre.find(genre_id.to_i)
+    end
+    @song = Song.create(name: params[:song][:name], artist_id: song_artist_id)
+    redirect '/songs/:slug'
+  end
+
 #READ
 
   get '/songs' do
@@ -11,11 +28,5 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     erb :'songs/show'
   end
-
-  #/songs/new ---
-
-  #/songs/:slug --- song show page
-  ####          --- should have links to that song's artist and each genre to song
-
 
 end
